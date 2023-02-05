2e6abaa334fc73077986733d9104204a50581c3d
Thiago Pereira Magalhaes <magalhaes.thiagopm@outlook.com>
2023-02-05 20:51:54 -0300

    Init

---
+++ bin/d.dart
@@ -0,0 +1,5 @@
+import 'package:d/d.dart' as d;
+
+void main(List<String> arguments) {
+  print('Hello world: ${d.calculate()}!');
+}
---
+++ test/d_test.dart
@@ -0,0 +1,8 @@
+import 'package:d/d.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
